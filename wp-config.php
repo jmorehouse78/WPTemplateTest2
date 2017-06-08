3f09a73719aabbc6ec2aca0973169cc85ca96d45
Jeremy <jeremymorehouse.gmail.com>
2017-06-07 20:54:23 -0400

    changed db to postgre

--- wp-config.php
+++ wp-config.php
@@ -18,25 +18,32 @@
  * @package WordPress
  */
 
+// ** Heroku Postgres settings - from Heroku Environment ** //
+//$db = parse_url($_ENV["DATABASE_URL"]);
+if (isset($_ENV["DATABASE_URL"]))
+  $db = parse_url($_ENV["DATABASE_URL"]);
+else
+  $db = parse_url($_SERVER["DATABASE_URL"]);
+
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', trim($db["path"],"/"));
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', $db["user"]);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', $db["pass"]);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', $db["host"]);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
